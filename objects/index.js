e2290feb49c223c86d3a1d1aa8a3ca1543889863
34x340 <114092241+34x340@users.noreply.github.com>
2023-12-20 21:06:44 +0200

    destructuring

--- objects/index.js
+++ objects/index.js
@@ -1,26 +1,80 @@
-// const playList = {
+// const playlist = {
 //     playListName: "My favorite songs",
 //     genre: ["Rock", "Pop"],
 //     songs: [
-//       {
-//         songName: "Пес Патрон",
-//         singer: "KARTA SVITU",
-//         year: 2023,
-//         isPopular: true
-//       },
-//       {
-//         songName: "BAIXO",
-//         singer: "xxanteria",
-//         year: 2023,
-//         isPopular: true
-//       },
-//       {
-//         songName: "Місто",
-//         singer: "onuka",
-//         year: 2015,
-//         isPopular: false
-//       }
+//         {
+//             songName: "Пес Патрон",
+//             singer: "KARTA SVITU",
+//             year: 2023,
+//             isPopular: true
+//         },
+//         {
+//             songName: "BAIXO",
+//             singer: "xxanteria",
+//             year: 2023,
+//             isPopular: true
+//         },
+//         {
+//             songName: "Місто",
+//             singer: "onuka",
+//             year: 2015,
+//             isPopular: false
+//         },
 //     ],
-//     songsQuantity: 1000000000
-//   }
+//     songsQuantity: {
+//         newSongs: 50,
+//         oldSongs: {
+//             popularSongs: 100,
+//         }
+//     },
+//     showplaylist() {
+//         alert(this.playListName);
+//     },
+//     addSong(songInfo) {
+//         this.songs.push(songInfo)
+//     },
+//     countSongs() {
+//         return this.songs.length;  
+//     },
+//     addGenre(genre) {
+//         this.genre.push(genre);
+//     },
+//     deleteSong(songNameToDel) {
+//         this.songs.forEach(song => {
+//             if (song.songName === songNameToDel) {
+//                 this.songs.splice(this.songs.indexOf(song),1);
+//             }
+//         });
+//     },
+//     countPopularSongs() {
+//         alert(this.songsQuantity.oldSongs.popularSongs);
+//     }
+// }
 
+// console.log(playlist, playlist.countSongs());
+
+// playlist.deleteSong("BAIXO");
+// // playlist.addGenre('Contry')
+
+// console.log(playlist.playListName)
+// console.log(playlist.location.city)
+// const city = playlist.location.city
+// console.log(city)
+// const {playListName} = playlist
+// console.log(playListName)
+// console.log(playListName)
+// const {rating = 0} = playlist
+// console.log(rating)
+
+const user = {
+    name: 'Joe',
+    location: {
+        country: 'Ukraine',
+        city: 'Lviv',
+    },
+    password: 1234
+}
+
+const {name: userName, location: {country: userCountry, city: userCity}, password: userPassword, follower: userFollower = 0} = user
+
+console.log(userName, userCountry, userCity, userPassword, userFollower)
