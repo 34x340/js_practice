e5777f275a4afdc7923835066eda3b0d937aa86d
34x340 <114092241+34x340@users.noreply.github.com>
2024-01-31 19:28:41 +0200

    practice

---
+++ ДОМ/dom.js
@@ -0,0 +1,27 @@
+// console.log(document.querySelector('ul'));
+
+// const galleryList = document.querySelector('gallery__list')
+
+// console.log(galleryList)
+
+const lists = document.querySelectorAll('ul')
+
+console.log(lists)
+
+console.log(lists[1])
+
+const text = document.querySelectorAll('.gallery__list .text')
+
+console.log(text)
+
+const checkBoxElement = document.querySelector('[type=checkbox]')
+
+console.log(checkBoxElement)
+
+// const idTest = document.querySelector('#test')
+
+// console.log(idTest)
+
+const idTest = document.getElementById('test')
+
+console.log(idTest)
